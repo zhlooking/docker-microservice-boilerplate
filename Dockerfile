a07563e1c084a793cb2e7ad5378114532f3be995
zhanghao <zhanghao@zcool.com.cn>
2017-12-12 02:26:57 +0800

    Add test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.6.3-alpine
+FROM python:3.6.3
 
 # set working directory
 RUN mkdir -p /usr/src/app
--- manage.py
+++ manage.py
@@ -1,8 +1,26 @@
+import unittest
 from flask_script import Manager
 
-from project import app
+from project import app, db
 
 manager = Manager(app)
+
+@manager.command
+def recreate_db():
+    """Recreate database"""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@manager.command
+def test():
+    """Runs the tests without code coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
 
 if __name__ == '__main__':
     manager.run()
